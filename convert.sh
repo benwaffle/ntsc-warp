93ad5e714f58fc239384afa979269400c4f09d05
Ben Iofel <iofelben@gmail.com>
2019-03-24 17:17:59 -0400

    better script

--- convert.sh
+++ convert.sh
@@ -2,17 +2,25 @@
 
 export OCL_ICD_VENDORS=mesa # radeon is broken
 
+#WIDTH=128
+#HEIGHT=96
+WIDTH=32
+HEIGHT=96
+
 if [ $# != 1 ]; then
     echo "./convert.sh <image.jpg>"
     exit 1
 fi
 
-BYTES=$(convert $1 -resize 96x128 -rotate 270 -colorspace GRAY txt:- \
-        | grep -ve "^#" \
-        | sed -r "s/^.*gray\(([0-9]+)\).*$/\1/" \
-        | while read num; do printf "0x%x\n" $num; done \
-        | tr '\n' ',' \
-        | sed 's/,$//')
+i=0
+BYTES=$(convert $1 -resize ${HEIGHT}x${WIDTH} -rotate 270 -colorspace GRAY -alpha off txt:- |
+        grep -ve "^#"                         |
+        sed -r "s/^.*gray\(([0-9]+)\).*$/\1/" |
+        while read num; do
+            printf "0x%x, " $num
+            ((i++))
+            (( $i % $WIDTH == 0 )) && printf "\n"
+        done                                  |
+        sed 's/,$//')
 
-echo "#include <stdint.h>
-static const uint8_t image[] = { $BYTES };" > include/image.h
+echo "PROGMEM static const unsigned char image[] = { $WIDTH, $HEIGHT, $BYTES };" > include/image.h
