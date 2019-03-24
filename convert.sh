cd1d53f6926be2dbef6a180e24bac21dc2934fe1
Ben Iofel <iofelben@gmail.com>
2019-03-24 19:07:28 -0400

    best script

--- convert.sh
+++ convert.sh
@@ -2,9 +2,7 @@
 
 export OCL_ICD_VENDORS=mesa # radeon is broken
 
-#WIDTH=128
-#HEIGHT=96
-WIDTH=32
+WIDTH=128
 HEIGHT=96
 
 if [ $# != 1 ]; then
@@ -13,14 +11,24 @@ if [ $# != 1 ]; then
 fi
 
 i=0
-BYTES=$(convert $1 -resize ${HEIGHT}x${WIDTH} -rotate 270 -colorspace GRAY -alpha off txt:- |
+#CMD="convert $1 -resize ${HEIGHT}x${WIDTH}! -rotate 270 -colorspace GRAY -alpha off"
+CMD="convert $1 -rotate 270 -colorspace GRAY -alpha off"
+
+$CMD small.jpg
+# BYTES=$(convert $1 -resize ${HEIGHT}x${WIDTH} -rotate 270 -colorspace GRAY -alpha off txt:- |
+BYTES=$($CMD txt:- |
         grep -ve "^#"                         |
         sed -r "s/^.*gray\(([0-9]+)\).*$/\1/" |
         while read num; do
-            printf "0x%x, " $num
-            ((i++))
             (( $i % $WIDTH == 0 )) && printf "\n"
+            (( $i % 8 == 0 )) && printf ", 0b"
+            if (( $num > 125 )); then
+                printf 1
+            else
+                printf 0
+            fi
+            ((i++))
         done                                  |
         sed 's/,$//')
 
-echo "PROGMEM static const unsigned char image[] = { $WIDTH, $HEIGHT, $BYTES };" > include/image.h
+echo "PROGMEM static const unsigned char image[] = { $WIDTH, $HEIGHT $BYTES };" > include/image.h
